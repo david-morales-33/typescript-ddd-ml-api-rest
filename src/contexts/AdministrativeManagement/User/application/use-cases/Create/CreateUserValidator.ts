03df4fb1f1d3f3bcf60cbecc81cd8a2d345a78eb
David Morales <analistametodos@marrocar.local>
2024-08-13 14:20:36 -0500

    Integracion de validador para crear usuario (caso de uso)

---
+++ src/contexts/AdministrativeManagement/User/application/use-cases/Create/CreateUserValidator.ts
@@ -0,0 +1,26 @@
+import { UserPermissionValidator } from "../../../../UserPermission/application/Validate/UserPermissionValidator";
+import { UserPermission } from "../../../../UserPermission/domain/entities/UserPermission";
+import { UserPermissionRepository } from "../../../../UserPermission/domain/repositories/UserPermissionRepository";
+import { UserPermissionId } from "../../../../UserPermission/domain/value-objects/UserPermissionId";
+import { UserPermissionLabel } from "../../../../UserPermission/domain/value-objects/UserPermissionLabel";
+import { UserId } from "../../../domain/value-objects/UserId";
+import { UserExternalService } from "../../services/UserExternalService";
+
+
+export class CreateUserValidator {
+    constructor(
+        private userPermissionsRepository: UserPermissionRepository,
+    ) { }
+
+    async execute(openByUser: UserId) {
+        const eventPermission = UserPermission.create(
+            new UserPermissionId(4),
+            new UserPermissionLabel('Crear Cuenta')
+        );
+        const userPermissionValidator = new UserPermissionValidator(this.userPermissionsRepository);
+        await userPermissionValidator.execute({
+            userId: openByUser,
+            useCasePermission: eventPermission
+        });
+    }
+}
