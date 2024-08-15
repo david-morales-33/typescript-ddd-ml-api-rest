59a2c38e1134c5fe86afd4b50e0f1aa02c9b5c6d
David Morales <analistametodos@marrocar.local>
2024-08-15 07:46:01 -0500

    Integracion de actualizador para  la entidad <Usuario>

--- src/contexts/AdministrativeManagement/User/domain/repositories/AuthUserRepository.ts
+++ src/contexts/AdministrativeManagement/User/domain/repositories/AuthUserRepository.ts
@@ -1,5 +1,7 @@
 import { AuthUser } from "../entities/AuthUser";
+import { UserId } from "../value-objects/UserId";
 
 export interface AuthUserRepository {
-    save(user: AuthUser): Promise<void>
+    save(user: AuthUser): Promise<void>;
+    find(userId: UserId): Promise<AuthUser| null | undefined>;
 }
