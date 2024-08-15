943fdc5f33721c65a64b6195af9262cfe5149bc3
David Morales <analistametodos@marrocar.local>
2024-08-15 08:43:26 -0500

    Integracion de validador para deshabilitar cuentas

---
+++ src/contexts/AdministrativeManagement/User/application/use-cases/Disable/DisableUserValidator.ts
@@ -0,0 +1,36 @@
+import { UserRepository } from "../../../../../SewingProductionAreaManagement/User/domain/repositories/UserRepository";
+import { UserPermissionValidator } from "../../../../UserPermission/application/Validate/UserPermissionValidator";
+import { UserPermission } from "../../../../UserPermission/domain/entities/UserPermission";
+import { UserPermissionRepository } from "../../../../UserPermission/domain/repositories/UserPermissionRepository";
+import { UserPermissionId } from "../../../../UserPermission/domain/value-objects/UserPermissionId";
+import { UserPermissionLabel } from "../../../../UserPermission/domain/value-objects/UserPermissionLabel";
+import { CommonUserRepository } from "../../../domain/repositories/CommonUserRepository";
+import { UserId } from "../../../domain/value-objects/UserId";
+import { UserAlreadyExistException } from "../../exceptions/UserAlreadyExistException";
+
+export class DisableUserValidator {
+    constructor(
+        private userPermissionsRepository: UserPermissionRepository,
+        private commonUserRepository: CommonUserRepository
+    ){}
+
+    async execute(params : {createBy: UserId, userId: UserId}) {
+
+        const { createBy, userId } = params;
+
+        const eventPermission = UserPermission.create(
+            new UserPermissionId(5),
+            new UserPermissionLabel('Editar Cuenta')
+        );
+
+        const userFinded = await this.commonUserRepository.find(userId);
+        if (userFinded !== null || userFinded !== undefined)
+            throw new UserAlreadyExistException(userId.value)
+
+        const userPermissionValidator = new UserPermissionValidator(this.userPermissionsRepository);
+        await userPermissionValidator.execute({
+            userId: createBy,
+            useCasePermission: eventPermission
+        });
+    }
+}
