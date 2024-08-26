6a22f43984154d785febe81a29fd80eb177a0e95
David Morales <analistametodos@marrocar.local>
2024-08-26 11:01:26 -0500

    Integracion de entidad <Operation>

---
+++ src/contexts/PlatformsManagement/menu/domain/entities/MenuContainerForMenues.ts
@@ -0,0 +1,8 @@
+import { MenuRoot } from "../interfaces/MenuRoot";
+
+export class MenuContainerForMenues implements MenuRoot {
+    toPrimitives() {
+
+    }
+}
+
---
+++ src/contexts/PlatformsManagement/menu/domain/interfaces/menuRoot.ts
@@ -0,0 +1,4 @@
+
+export abstract class MenuRoot {
+    abstract toPrimitives(): any;
+} 
