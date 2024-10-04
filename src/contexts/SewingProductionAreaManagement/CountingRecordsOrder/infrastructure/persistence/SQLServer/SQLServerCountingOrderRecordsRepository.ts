3810ec0ec041120a17181ce73012d359dfbf1bc4
David Morales <analistametodos@marrocar.local>
2024-10-04 12:51:54 -0500

    Integracion de entidades SQL en contenedor de dependencias

---
+++ src/contexts/SewingProductionAreaManagement/CountingRecordsOrder/infrastructure/persistence/SQLServer/SQLServerCountingOrderRecordsRepository.ts
@@ -0,0 +1,34 @@
+import { SQLServerRepository } from "../../../../../Shared/infrastructure/persistence/SQLServere/SQLServerRepository";
+import { CountingRecordsOrderViewDTO } from "../../../domain/data-transfer-object/CountingRecordsOrderViewDTO";
+import { CountingRecordsOrderResponseRepository } from "../../../domain/repositories/CountingRecordsOrderResponseRepository";
+import { CountingRecordsOrderId } from "../../../domain/value-objects/CountingRecordsOrderId";
+
+
+export class SQLServerCountingOrderRecordsRepository extends SQLServerRepository implements CountingRecordsOrderResponseRepository {
+
+    protected procedureStoreName(): string {
+        return 'sp_gestion_ml_db_produccion_solicitud_ocr_lista_completa';
+    }
+
+    async find(countingRecordsOrderId: CountingRecordsOrderId): Promise<CountingRecordsOrderViewDTO | null> {
+        console.log(countingRecordsOrderId)
+        try {
+            const result = await this.execute([])
+            console.log(result)
+        } catch (error) {
+            console.log(error)
+        } finally {
+            this.disconnection();
+        }
+        return new CountingRecordsOrderViewDTO('uaays-asas-a4s7-ruhebebaas-ttgtd',
+            'MOP3547', 'MAR3548', 'XL', '1000', 'BLANCO', 80, '7777844544545', '12:20:14', '01:15:03', 1, 'PRIMERA', 1, 'MODULO-1', true, null, null, '1146441925', 'David Morales', new Date(), null, null, null, null)
+    }
+
+    async match(criteria: any): Promise<CountingRecordsOrderViewDTO[]> {
+        console.log(criteria)
+        return [
+            new CountingRecordsOrderViewDTO('uaays-asas-a4s7-ruhebebaas-ttgtd',
+                'MOP3547', 'MAR3548', 'XL', '1000', 'BLANCO', 80, '7777844544545', '12:20:14', '01:15:03', 1, 'PRIMERA', 1, 'MODULO-1', true, null, null, '1146441925', 'David Morales', new Date(), null, null, null, null),
+        ]
+    }
+}
--- src/entities-test/console.integration.test.ts
+++ src/entities-test/console.integration.test.ts
@@ -1,6 +1,23 @@
+import { container } from "../server/SewingProductionAreaManagement/dependency-inyection/application";
+import { SQLServerCountingOrderRecordsRepository } from '../contexts/SewingProductionAreaManagement/CountingRecordsOrder/infrastructure/persistence/SQLServer/SQLServerCountingOrderRecordsRepository'
+import { CountingRecordsOrderId } from "../contexts/SewingProductionAreaManagement/CountingRecordsOrder/domain/value-objects/CountingRecordsOrderId";
+import { } from 'uuid'
+import { SQLServerPoolFactory } from "../contexts/Shared/infrastructure/persistence/SQLServere/SQLServerPoolFactory";
 import { SQLServerConfigFactory } from "../contexts/SewingProductionAreaManagement/Shared/infrastructure/persistence/SQLServer/SQLServerConfigFactory";
-import { EnviromentConfig } from "../server/EnviromentConfig";
 
-const config = SQLServerConfigFactory.createConfig();
+const repo = container.get<SQLServerCountingOrderRecordsRepository>('SewingProductionAreaManagement.infrastructure.CountingRecordsOrder.SqlServerCountingOrderRecordsRepository');
 
-console.log(config)
+async function query() {
+    try {
+        const id = new CountingRecordsOrderId(CountingRecordsOrderId.random().value);
+        await repo.find(id)
+        // const pool = await container.get('SewingProductionAreaManagement.infrastructure.shared.ConnectionManager')
+
+        // const pool = await SQLServerPoolFactory.createPool('SewingProductionAreaManagement',SQLServerConfigFactory.createConfig())
+        // console.log(pool)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+query();
