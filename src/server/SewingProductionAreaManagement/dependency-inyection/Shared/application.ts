142c8052c909e4db78ac0f709f4d282a473ab39b
david-morales-33 <davidmorales33.97@gmail.com>
2024-10-05 09:04:43 -0500

    marge

--- src/server/SewingProductionAreaManagement/dependency-inyection/Shared/application.ts
+++ src/server/SewingProductionAreaManagement/dependency-inyection/Shared/application.ts
@@ -1,8 +1,3 @@
 import { container } from '../application';
-import { SQLServerPoolFactory } from '../../../../contexts/Shared/infrastructure/persistence/SQLServere/SQLServerPoolFactory';
 
-// container.register();
 
-// container.register('Shared.ConnectionManager', SQLServerPoolFactory).
-// addArgument('SewingProductionArea').
-// addArgument(container.get(''));
