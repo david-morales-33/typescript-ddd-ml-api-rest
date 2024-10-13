64973a8d1a1db74b683053072c41dde3f54d3d45
david-morales-33 <davidmorales33.97@gmail.com>
2024-10-13 12:48:30 -0500

    Integracion de caso de uso para la creacion de ordenes de produccion <Actualizar>

--- src/contexts/SewingProductionAreaManagement/ProductionOrder/domain/repositories/ProductionOrderCommandRepository.ts
+++ src/contexts/SewingProductionAreaManagement/ProductionOrder/domain/repositories/ProductionOrderCommandRepository.ts
@@ -1,6 +1,5 @@
-import { ProductionOrderInProgress } from "../entities/ProductionOrderInProgress";
 import { ProductionOrderNotStarted } from "../entities/ProductionOrderNotStarted";
 
 export interface ProductionOrderCommandRepository {
-    save(productionOrder: ProductionOrderNotStarted | ProductionOrderInProgress): Promise<void>;
+    save(productionOrder: ProductionOrderNotStarted): Promise<void>;
 }
