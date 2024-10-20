13c3b50ff622a4c2e613f5dcfec1fde5bdae9db8
david-morales-33 <davidmorales33.97@gmail.com>
2024-10-20 14:08:31 -0500

    Integracion de caso de uso para la utenticacion por medio de token

---
+++ src/contexts/Authentication/AuthenticationUser/application/AccessByToken/UserAuthenticatorByToken.ts
@@ -0,0 +1,36 @@
+import { UserId } from "../../../../Shared/domain/value-object/UserId";
+import { AuthenticationToken } from "../../../AuthenticationToken/domain/entity/AuthenticationToken";
+import { AuthenticationTokenService } from "../../../AuthenticationToken/domain/services/AuthenticationTokenService";
+import { TokenCreationDate } from "../../../AuthenticationToken/domain/value-objects/TokenCreationDate";
+import { TokenExpirationDate } from "../../../AuthenticationToken/domain/value-objects/TokenExpirationDate";
+import { TokenId } from "../../../AuthenticationToken/domain/value-objects/TokenId";
+import { UserRepository } from "../../domain/repositories/UserRepository";
+import { AuthenticationResponse } from "../AccessByCredentials/AuthenticationResponse";
+
+export class UserAuthenticatorByToken {
+    constructor(
+        private userRepository: UserRepository,
+        private tokenService: AuthenticationTokenService
+    ) { }
+    async execute(token: TokenId) {
+
+        const data = await this.tokenService.deserialize(token);
+
+        if (data === null) throw new Error('Token no válido');
+
+        if (new Date() > data.expirationDate) throw new Error('El token ha experido');
+
+        const user = await this.userRepository.find(new UserId(data.userId));
+
+        if (user === null) throw new Error('El usuario solicitado no existe');
+
+        return new AuthenticationResponse(
+            user,
+            new AuthenticationToken(
+                token,
+                new TokenExpirationDate(data.expirationDate),
+                new TokenCreationDate(data.craetionDate)
+            )
+        );
+    }
+}
