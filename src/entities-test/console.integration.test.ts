b9425969175bef8d133936b52336c3d520bedd74
David Morales <analistametodos@marrocar.local>
2024-10-23 15:27:55 -0500

    Integracion de servicio en contenedor para exponer vista de orden de produccion/detalle en contexto productivo

--- src/entities-test/console.integration.test.ts
+++ src/entities-test/console.integration.test.ts
@@ -1,16 +1,16 @@
 import { v4 as uuid } from 'uuid';
-import { container } from '../server/Authentication/dependency-inyection/application'
-import { UserAuthenticatorByCredentialsQuery } from '../contexts/Authentication/AuthenticationUser/application/AccessByCredentials/UserAuthenticatorByCredentialsQuery';
-import { UserAuthenticatorByTokenQueryHandler } from '../contexts/Authentication/AuthenticationUser/application/AccessByToken/UserAuthenticatorByTokenQueryHandler';
-import { UserAuthenticatorByTokenQuery } from '../contexts/Authentication/AuthenticationUser/application/AccessByToken/UserAuthenticatorByTokenQuery';
+import { container } from '../server/SewingProductionAreaManagement/dependency-inyection/application'
+import { ProductionOrderDetailQueryHandler } from '../contexts/SewingProductionAreaManagement/ProductionOrderDetail/application/use-cases/Find/ProductionOrderDetailQueryHandler';
+import { FindProductionOrderDetailQuery } from '../contexts/SewingProductionAreaManagement/ProductionOrderDetail/application/use-cases/Find/FindProductionOrderDetailQuery';
+
 
 async function query() {
     try {
-        const handler = container.get<UserAuthenticatorByTokenQueryHandler>('Authentication.infrastructure.User.UserAuthenticatorByTokenQueryHandler');
+        const handler = container.get<ProductionOrderDetailQueryHandler>('SewingProductionAreaManagement.application.ProductionOrderDetail.ProductionOrderDetailQueryHandler');
 
-        const credentials = new UserAuthenticatorByTokenQuery('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiIxMTQ2NDQxOTI1IiwicHJvZmlsZUlkIjoxLCJpYXQiOjE3Mjk3MTI1OTUsImV4cCI6MTczMjM0MjM5NX0.1eJClc3kb6wDc37R0Cd3NYpcvyslVIPyO1FDReiHat3opL1SR0wM1jlaxReE2oFMSHUaFbr-mJuzuP_052xtBhAoxn4lohhwoSbH8YN22POu_J57skm5c-fQ4sKArNfMFJT3pHqHvQMo47RaZKAHu693kamjuMC9Sr9LLgEfu_7DVbMyg-MAmB6sKDZOo-zkfwte8dfFP42E1suQnLp4qVvt1uoqu6bov6-2PLV7xZOgBGFi5VJj0YA3HDtpc2GCCfRmnpzS6qaDSfY1tpTB1r8v4sYa6k6Yt0Dfi2tBK_clXV0lAomz3Au_NiHizNTLfp0Cc3YJL-BWHJmTVDZnDg')
+        const query = new FindProductionOrderDetailQuery('MOB4399')
+        const response = await handler.handle(query)
 
-        const response = await handler.handle(credentials);
         console.log(response)
     } catch (error) { console.log(error) }
 }
--- src/server/SewingProductionAreaManagement/dependency-inyection/ProductionOrderDetail/application.ts
+++ src/server/SewingProductionAreaManagement/dependency-inyection/ProductionOrderDetail/application.ts
@@ -1,6 +1,16 @@
+import { ProductionOrderDetailFinder } from '../../../../contexts/SewingProductionAreaManagement/ProductionOrderDetail/application/use-cases/Find/ProductionOrderDetailFinder';
+import { ProductionOrderDetailQueryHandler } from '../../../../contexts/SewingProductionAreaManagement/ProductionOrderDetail/application/use-cases/Find/ProductionOrderDetailQueryHandler';
 import { SQLServerProductionOrderDetail } from '../../../../contexts/SewingProductionAreaManagement/ProductionOrderDetail/infrastructure/Persistence/SQLServer/SQLServerProductionOrderDetail';
 import { container } from '../application';
 
-const sqlServerProductionOrderDetail = container.register('SewingProductionAreaManagement.infrastructure.ProductionOrderDetail.SqlServerProductionOrderDetail', SQLServerProductionOrderDetail).
+container.
+    register('SewingProductionAreaManagement.infrastructure.ProductionOrderDetail.SqlServerProductionOrderDetail', SQLServerProductionOrderDetail).
     addArgument(container.get('SewingProductionAreaManagement.infrastructure.shared.ConnectionManager'))
 
+container.
+    register('SewingProductionAreaManagement.application.ProductionOrderDetail.ProductionOrderDetailFinder', ProductionOrderDetailFinder).
+    addArgument(container.get('SewingProductionAreaManagement.infrastructure.ProductionOrderDetail.SqlServerProductionOrderDetail'));
+
+container.
+    register('SewingProductionAreaManagement.application.ProductionOrderDetail.ProductionOrderDetailQueryHandler', ProductionOrderDetailQueryHandler).
+    addArgument(container.get('SewingProductionAreaManagement.application.ProductionOrderDetail.ProductionOrderDetailFinder'));
