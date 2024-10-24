3432efbe5955ab68acf89cb38b9f9c0dab5b3dd2
David Morales <analistametodos@marrocar.local>
2024-10-24 16:02:37 -0500

    Integracion de entidad <ProductionOrderPlannedSewingProcess>

---
+++ src/contexts/SharedAdministrativeManagement/ProductionOrderSewingProcess/domain/data-transfer-objects/ProductionOrderPlannedSewingProcessDTO.ts
@@ -0,0 +1,18 @@
+export class ProductionOrderPlannedSewingProcessDTO {
+    private _className: string = 'ProductionOrder.PlannedSewingProcessDTO';
+    constructor(
+        public readonly productionOrderId: string,
+        public readonly proccessId: number,
+        public readonly productionOrderType: string,
+        public readonly executedAmount: number,
+        public readonly plannedAmount: number,
+        public readonly plannedProductionModuleId: number,
+        public readonly plannedStartDate: Date,
+        public readonly plannedEndDate: Date,
+        public readonly executedStartDate: Date | null,
+        public readonly executedEndDate: Date | null,
+    ) { }
+    public get className(): string {
+        return this._className;
+    }
+}
--- src/contexts/SharedAdministrativeManagement/ProductionOrderSewingProcess/domain/entities/ProductionOrderPlannedSewingProcess.ts
+++ src/contexts/SharedAdministrativeManagement/ProductionOrderSewingProcess/domain/entities/ProductionOrderPlannedSewingProcess.ts
@@ -7,30 +7,77 @@ import { ProductionOrderProcessStartDate } from "../../../../Shared/domain/value
 import { ProductionOrderProcessStartDatePlanned } from "../../../../Shared/domain/value-object/ProductionOrderProcessStartDatePlanned";
 import { ProductionOrderProcessId } from "../../../Shared/domain/value-objects/ProductionOrderProcessId";
 import { ProductionOrderType } from "../../../Shared/domain/value-objects/ProductionOrderType";
+import { ProductionOrderPlannedSewingProcessDTO } from "../data-transfer-objects/ProductionOrderPlannedSewingProcessDTO";
 import { ProductionOrderSewingProcess } from "../interfaces/ProductionOrderSewingProcess";
+import { ProductionModuleId } from "../value-objects/ProductionModuleId";
 
-export class ProductionOrderPlannedSewingProcess implements ProductionOrderSewingProcess{
-    private _plannedProductionModuleId: any;
-    private _plannedStartDate: ProductionOrderProcessStartDatePlanned | null = null;
-    private _plannedEndDate: ProductionOrderProcessEndDatePlanned | null;
-
+export class ProductionOrderPlannedSewingProcess implements ProductionOrderSewingProcess {
     constructor(
         readonly productionOrderId: ProductionOrderId,
         readonly proccessId: ProductionOrderProcessId,
         readonly productionOrderType: ProductionOrderType,
         readonly executedAmount: ProductionOrderExecutedAmount,
         readonly plannedAmount: ProductionOrderPlannedAmount,
+        readonly plannedProductionModuleId: ProductionModuleId,
+        readonly plannedStartDate: ProductionOrderProcessStartDatePlanned,
+        readonly plannedEndDate: ProductionOrderProcessEndDatePlanned,
         readonly executedStartDate: ProductionOrderProcessStartDate | null,
         readonly executedEndDate: ProductionOrderProcessEndDate | null
     ) { }
 
-    // static create(): ProductionOrderSewingProcess {
-    //     return new ProductionOrderSewingProcess(
+    static create(
+        productionOrderId: ProductionOrderId,
+        proccessId: ProductionOrderProcessId,
+        productionOrderType: ProductionOrderType,
+        executedAmount: ProductionOrderExecutedAmount,
+        plannedAmount: ProductionOrderPlannedAmount,
+        plannedProductionModuleId: ProductionModuleId,
+        plannedStartDate: ProductionOrderProcessStartDatePlanned,
+        plannedEndDate: ProductionOrderProcessEndDatePlanned,
+        executedStartDate: ProductionOrderProcessStartDate | null,
+        executedEndDate: ProductionOrderProcessEndDate | null
+    ): ProductionOrderPlannedSewingProcess {
+        return new ProductionOrderPlannedSewingProcess(
+            productionOrderId,
+            proccessId,
+            productionOrderType,
+            executedAmount,
+            plannedAmount,
+            plannedProductionModuleId,
+            plannedStartDate,
+            plannedEndDate,
+            executedStartDate,
+            executedEndDate
+        )
+    }
 
-    //     )
-    // }
+    static fromPrimitives(data: ProductionOrderPlannedSewingProcessDTO): ProductionOrderPlannedSewingProcess {
+        return new ProductionOrderPlannedSewingProcess(
+            new ProductionOrderId(data.productionOrderId),
+            new ProductionOrderProcessId(data.proccessId),
+            new ProductionOrderType(data.productionOrderType),
+            new ProductionOrderExecutedAmount(data.executedAmount),
+            new ProductionOrderPlannedAmount(data.plannedAmount),
+            new ProductionModuleId(data.plannedProductionModuleId),
+            new ProductionOrderProcessStartDatePlanned(data.plannedStartDate),
+            new ProductionOrderProcessEndDatePlanned(data.plannedEndDate),
+            data.executedStartDate ? new ProductionOrderProcessStartDate(data.executedStartDate) : null,
+            data.executedEndDate ? new ProductionOrderProcessEndDate(data.executedEndDate) : null
+        );
+    }
 
-    toPrimitives() {
-        
+    toPrimitives(): ProductionOrderPlannedSewingProcessDTO {
+        return new ProductionOrderPlannedSewingProcessDTO(
+            this.productionOrderId.value,
+            this.proccessId.value,
+            this.productionOrderType.value,
+            this.executedAmount.value,
+            this.plannedAmount.value,
+            this.plannedProductionModuleId.value,
+            this.plannedStartDate.value,
+            this.plannedEndDate.value,
+            this.executedStartDate ? this.executedStartDate.value : null,
+            this.executedEndDate ? this.executedEndDate.value : null,
+        )
     }
 }
